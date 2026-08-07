4c6dca19c8c25ac06246083e0103fdd26019c367
rayong <skdyds@gmail.com>
2026-08-07 13:52:26 +0900

    fix: improve OAuth callback postMessage handling

--- api/callback.js
+++ api/callback.js
@@ -2,24 +2,40 @@ export default async function handler(req, res) {
   const { code } = req.query;
   const { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET } = process.env;
 
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    body: JSON.stringify({ client_id: OAUTH_CLIENT_ID, client_secret: OAUTH_CLIENT_SECRET, code }),
-  });
+  try {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+      body: JSON.stringify({ client_id: OAUTH_CLIENT_ID, client_secret: OAUTH_CLIENT_SECRET, code }),
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (data.access_token) {
-    const script = `
-<script>
-  const msg = JSON.stringify({ token: "${data.access_token}", provider: "github" });
-  window.opener.postMessage("authorization:github:success:" + msg, "*");
-  window.close();
-</script>`;
-    res.setHeader('Content-Type', 'text/html');
-    res.send(script);
-  } else {
-    res.status(400).send('OAuth failed');
+    if (data.access_token) {
+      const token = data.access_token;
+      const content = JSON.stringify({ token, provider: 'github' });
+      const message = `authorization:github:success:${content}`;
+      res.setHeader('Content-Type', 'text/html');
+      res.send(`<!DOCTYPE html><html><body><script>
+        (function() {
+          var msg = ${JSON.stringify(message)};
+          if (window.opener) {
+            window.opener.postMessage(msg, '*');
+          }
+          window.close();
+        })();
+      <\/script></body></html>`);
+    } else {
+      res.setHeader('Content-Type', 'text/html');
+      res.send(`<!DOCTYPE html><html><body><script>
+        (function() {
+          var msg = 'authorization:github:error:' + JSON.stringify(${JSON.stringify(JSON.stringify(data))});
+          if (window.opener) { window.opener.postMessage(msg, '*'); }
+          window.close();
+        })();
+      <\/script></body></html>`);
+    }
+  } catch (e) {
+    res.status(500).send('Server error: ' + e.message);
   }
 }
